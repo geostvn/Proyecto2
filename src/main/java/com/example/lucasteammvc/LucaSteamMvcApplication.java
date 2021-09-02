6d1b15fabd602f402c5111f8a030e6634191c970
Julio <julio@ruedaferrero.com>
2021-09-02 15:44:52 +0200

    Task3 - CrearProyectoMaven2

---
+++ src/main/java/com/example/lucasteammvc/LucaSteamMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.lucasteammvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaSteamMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucaSteamMvcApplication.class, args);
+    }
+
+}
